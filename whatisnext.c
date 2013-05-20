c6456ded8bfb70b8f5f4e9013e956aaa520a235c
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2013-05-20 19:00:05 +0530

    What is next

---
+++ whatisnext.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+int a1,a2,a3;
+
+	scanf("%d %d %d",&a1,&a2,&a3);
+	
+	
+	if(a1==0 && a2==0 && a3==0)
+	{
+	return;
+	}
+
+	if((a2-a1) == (a3-a2))
+	{
+		printf("AP %d", a3 + (a3-a2));
+	}
+	else
+	{
+		printf("GP %d", a3*(a3/a2));
+	}
+
+	
+return 0;
+}
